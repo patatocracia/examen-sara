f2722e0dc0d35d861a89a0fec3cdadf60999c732
pablo <pablodeandresrivera@outlook.com>
2020-05-30 15:11:49 +0200

    todo

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+var app = express();
+
+//configuraciones
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+
+
+//link angular
+
+
+
+//cargar rutas
+
+
+
+//middlewares
+
+
+
+
+//cors
+
+
+
+
+//rutas
+
+
+
+
+//export
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = require('./app')
+
+
+//connect mongodb y servidor
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(()=>{
+          console.log("La conexion a la base de datos cesfuencarral_web se ha realizado correctamente...");
+          //crear servidor
+          app.listen(process.env.PORT || 5000, ()=>{
+          })
+        })
+        .catch(err => console.log(err));
