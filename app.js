c57310361738f3a9cc0cb651a0f32283eeee07a2
pablo <pablodeandresrivera@outlook.com>
2020-05-31 07:50:57 +0200

    s

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 //cargar rutas
-
+var pregunta_routes = require('./routes/pregunta');
 
 
 //middlewares
@@ -27,12 +27,19 @@ app.use(express.static(distDir));
 
 
 //cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
+  next();
+});
 
 
 
 //rutas
-
+app.use('/api', pregunta_routes);
 
 
 
