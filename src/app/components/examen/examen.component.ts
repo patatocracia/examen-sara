75a7e47a369a5a2c5a24427406a5ee489b38af40
pablo <pablodeandresrivera@outlook.com>
2020-05-31 21:28:52 +0200

    a

--- src/app/components/examen/examen.component.ts
+++ src/app/components/examen/examen.component.ts
@@ -17,6 +17,7 @@ export class ExamenComponent implements OnInit {
   constructor(private _preguntaService: PreguntaService) {
     this.numeroPreguntas = 10;
     this.status = false;
+    this.preguntas = new Array;
   }
 
   ngOnInit(): void {
